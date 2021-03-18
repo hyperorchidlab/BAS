a726bd2ac305c34b36135ff0dc4a5a43b7e865f2
megaliao <micliao@tutanota.com>
2021-03-18 15:30:18 +0800

    add to repo

--- cmd/bascmdservice.go
+++ cmd/bascmdservice.go
@@ -2,20 +2,34 @@ package cmd
 
 import (
 	"context"
-	"encoding/json"
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/hyperorchidlab/BAS/dbSrv"
 
 	"github.com/hyperorchidlab/BAS/pbs"
 )
 
+func toMinerString(record *dbSrv.Record) string {
+	msg := ""
+	msg += fmt.Sprintf("address: %s \r\n",string(record.BAddr))
+	msg += fmt.Sprintf("ip addr: %s \r\n",string(record.NAddr))
+	msg += fmt.Sprintf("extData: %s\r\n",record.ExtData)
+	return msg
+}
+
 func (cs *cmdService) ShowAllMiners(ctx context.Context, r *pbs.EmptyRequest) (*pbs.CommonResponse, error) {
 
-	msg := "no miners"
+	msg := ""
 
 	miners:=cs.book.FindAll(21,60)
 	for i:=0;i<len(miners);i++{
-		j,_:=json.MarshalIndent(miners[i]," ","\t")
-		msg += string(j)
-		msg += "================================================"
+
+		msg += toMinerString(miners[i])
+		msg += "================================================\r\n"
+	}
+
+	if msg == ""{
+		msg = "no miners"
 	}
 
 	return &pbs.CommonResponse{
@@ -24,14 +38,27 @@ func (cs *cmdService) ShowAllMiners(ctx context.Context, r *pbs.EmptyRequest) (*
 
 }
 
+func toPoolsString(record *dbSrv.Record) string  {
+	msg := ""
+	msg += fmt.Sprintf("address: %s \r\n",common.BytesToAddress(record.BAddr).String())
+	msg += fmt.Sprintf("ip addr: %s \r\n",string(record.NAddr))
+	msg += fmt.Sprintf("extData: %s\r\n",record.ExtData)
+
+	return msg
+}
+
 func (cs *cmdService) ShowAllPools(ctx context.Context, r *pbs.EmptyRequest) (*pbs.CommonResponse, error) {
-	msg := "no pools"
+	msg := ""
 
 	pools:=cs.book.FindAll(0,21)
 	for i:=0;i<len(pools);i++{
-		j,_:=json.MarshalIndent(pools[i]," ","\t")
-		msg += string(j)
-		msg += "================================================"
+
+		msg += toPoolsString(pools[i])
+		msg += "================================================\r\n"
+	}
+
+	if msg == ""{
+		msg = "no pools"
 	}
 
 	return &pbs.CommonResponse{
--- go.mod
+++ go.mod
@@ -3,13 +3,10 @@ module github.com/hyperorchidlab/BAS
 go 1.14
 
 require (
-	github.com/btcsuite/btcutil v1.0.2 // indirect
 	github.com/btcsuite/goleveldb v1.0.0
 	github.com/ethereum/go-ethereum v1.10.1
 	github.com/golang/protobuf v1.5.1
-	github.com/google/uuid v1.2.0 // indirect
 	github.com/hyperorchidlab/go-miner-pool v0.0.0-20210318021934-6f15dbd20195
-	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7 // indirect
 	github.com/spf13/cobra v1.1.3
 	github.com/tklauser/go-sysconf v0.3.5 // indirect
 	golang.org/x/crypto v0.0.0-20210317152858-513c2a44f670 // indirect
